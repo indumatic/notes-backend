eaeef5aa69ea9a0a3bf55fe6f25710ed48e18cc3
indumatic <electrico@indumatic.net>
2025-01-29 08:07:30 +0100

    uibuild

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ app.get('/api/notes/:id', (request, response, next) => {
       .catch(error => next(error))    
 })
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
     const body = request.body
 
     if (!body.content) return response.status(400).json({
@@ -57,6 +57,7 @@ app.post('/api/notes', (request, response) => {
     note.save().then(savedNote => {
       response.json(savedNote)
     })
+    .catch(error => next(error))
     
 })
 
@@ -68,15 +69,14 @@ app.delete('/api/notes/:id',(request, response) => {
 })
 
 app.put('/api/notes/:id', (request, response, next) => {
-  const body = request.body
-
-  const note = {
-    content: body.content,
-    important: body.important
-  }
+  const {content, important} = request.body
   
   Note
-    .findByIdAndUpdate(request.params.id, note, {new:true})
+    .findByIdAndUpdate(
+      request.params.id, 
+      {content, important}, 
+      {new:true, runValidators: true, context: 'query'}
+    )
     .then(updatedNote => {
       response.json(updatedNote)
     })
@@ -95,6 +95,10 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({error: 'malformatted id'})
+  }
+
+  if (error.name === 'ValidationError') {
+    return response.status(400).send({error: error.message})
   }
 
   next(error)
